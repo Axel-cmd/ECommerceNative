ec9b4cd5031f54f6ec0b1cfa4743982d3440f248
Axel-cmd <axelminart.pro@gmail.com>
2024-02-18 13:00:44 +0100

    Ajout de getter sur la classe article

--- src/models/articles.ts
+++ src/models/articles.ts
@@ -6,13 +6,13 @@ export type Articles = Article[];
 
 
 export class Article {
-
+    /** ID unique de l'article */
     private _id: string;
-
+    /** Nom de l'article */
     private _name: string;
-
+    /** Description de l'article */
     private _description: string;
-
+    /** prix par défaut de l'article (sans réduction/promotion) */
     private _default_price: number;
 
     constructor() {
@@ -21,6 +21,26 @@ export class Article {
         this._description   = "";
         this._default_price = 0
     }
+
+    //#region GETTER
+
+    public get id() : string {
+        return this._id;
+    }
+
+    public get name() : string {
+        return this._name;
+    }
+    
+    public get description() : string {
+        return this._description
+    }
+    
+    public get default_price() : number {
+        return this._default_price;
+    }
+    
+    //#endregion
 
     public loadFromJson(json: any) {
         Object.assign(this, {
