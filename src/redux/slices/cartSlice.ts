20d16c3781360eb432f397a7b22fa47a3c6123ea
Axel-cmd <axelminart.pro@gmail.com>
2024-02-21 19:39:02 +0100

    CartSlice suppression des traitements asynchrone dans le redux

--- src/redux/slices/cartSlice.ts
+++ src/redux/slices/cartSlice.ts
@@ -1,6 +1,4 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { auth } from "firebase";
-import { getUserDocumentByUid, updateUserDocument } from "src/api/users";
 
 const initialState: string[] = []
 
@@ -8,34 +6,20 @@ const cartSlice = createSlice({
     name: "cart",
     initialState,
     reducers: {
-        loadCartFromFirestore: (state) => {
-            if(auth.currentUser?.uid){
-                getUserDocumentByUid(auth.currentUser?.uid)
-                    .then(result => {
-                        state.push(...result.cart)
-                    })
-            }
+        loadCartList: (state, action) => {
+            state.push(...action.payload)
         },
         addItemToCart: (state, action) => {
             state.push(action.payload)
-
-            updateUserDocument({
-                cart: [...state]
-            })
         },
         removeItemFormCart: (state, action) => {
             const cart = state.filter((w: any) => w !== action.payload);
-
-            updateUserDocument({
-                cart: [...cart]
-            })
-
             return cart;
         }
     }
 })
 
-export const { addItemToCart, removeItemFormCart } = cartSlice.actions;
+export const { loadCartList, addItemToCart, removeItemFormCart } = cartSlice.actions;
 
 export const selectCart = (state: any) => state.cart;
 
