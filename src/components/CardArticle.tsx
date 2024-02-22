b604ae2b8bd95972ea5a7006466f1fc6e474c15a
LucasHervieu <lucashervieu0407@gmail.com>
2024-02-22 11:52:50 +0100

    product detail

--- src/components/CardArticle.tsx
+++ src/components/CardArticle.tsx
@@ -49,13 +49,14 @@ export const CardArticle = ({article}: Props) => {
     }
 
 
-    /**
-     * Récupérer l'image lors de la création du composant
-     */
+
     useEffect(() => {
+
         storage.ref(article.image).getDownloadURL()
             .then( r => {
+                console.log(r)
                 setImgUrl(r);
+
             })
     }, [])
 
@@ -63,42 +64,11 @@ export const CardArticle = ({article}: Props) => {
     return (
         <TouchableOpacity onPress={() => navigation.navigate('ProductDetail')}>
 
-            <Image
-                source={ { uri : imgUrl != '' ? imgUrl : undefined }} 
-                style={styles.image}
-            />
-                <View style={styles.articleText}>
-                    <Text style={styles.collection}>{article.collection}</Text>
-                    <Text style={styles.name}>{article.name}</Text> 
-                    <Text style={styles.price}>{article.defaultPrice} €</Text>
-                    
-                </View>
+            <View style={styles.articleItem}>
 
-                {/* <CheckBox
-                        containerStyle={{
-                            margin: 5,
-                            padding: 0
-                        }}
-                        size={18}
-                        checked={true}
-                        iconType="ionicon"
-                        checkedIcon="cart"
-                        uncheckedIcon="cart-outline"
-                        checkedColor="black"
-                        onPress={toggleAddRemoveFromWishlist}
-                    /> */}
-
-            
-
-            <View style={styles.heartContainer} >
-                <CheckBox
-                    size={18}
-                    containerStyle={styles.heart}
-                    checked={userWishList.includes(article.id)}
-                    checkedIcon="heart"
-                    uncheckedIcon="heart-o"
-                    checkedColor="red"
-                    onPress={toggleAddRemoveFromWishlist}
+                <Image
+                    source={ { uri : imgUrl }} 
+                    style={styles.image}
                 />
 
 
