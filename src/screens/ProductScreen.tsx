a9f500bcbf69dbe15bf77f5c74c2ab94cd25c17d
LucasHervieu <lucashervieu0407@gmail.com>
2024-02-22 15:49:42 +0100

    style

--- src/screens/ProductScreen.tsx
+++ src/screens/ProductScreen.tsx
@@ -104,7 +104,7 @@ const styles = StyleSheet.create({
   productImage: {
     width: '100%',
     height: 300,
-    resizeMode: 'cover',
+    resizeMode: 'contain',
   },
   productDetails: {
     padding: 20,
